3c60a2aee6a4d96dc2049a2e7fa0d8db9f2d9215
Bouchra KOUACH <bouchra.kouach@sii-maroc.com>
2022-09-30 14:56:05 +0100

    first commit

---
+++ src/test/java/projet5/TestProgram.java
@@ -0,0 +1,14 @@
+package projet5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public abstract class TestProgram {
+
+	@Test
+	public void testProgram() {
+		assertTrue(true);
+	}
+	
+}
